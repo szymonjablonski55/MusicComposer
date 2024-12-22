4aba24c586bed0c8340581a418562d2bc7da6ad5
szymonjablonski55 <jablonskiszymon55@gmail.com>
2024-12-22 12:55:29 +0100

    Add simple frontend

--- src/main/java/com/music/config/ProjectSecurityConfig.java
+++ src/main/java/com/music/config/ProjectSecurityConfig.java
@@ -1,4 +1,59 @@
 package com.music.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+
 public class ProjectSecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        // builder design pattern
+        http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/register").permitAll()
+
+        .anyRequest().authenticated()
+        )
+        .formLogin(loginConfigurer -> loginConfigurer
+                .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .logout(logoutConfigurer -> logoutConfigurer
+                        .logoutSuccessUrl("/login?logout=true")
+                                .invalidateHttpSession(true).permitAll()
+                        )
+        .httpBasic(withDefaults());
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("12345"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("54321"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
--- src/main/java/com/music/controller/HomeController.java
+++ src/main/java/com/music/controller/HomeController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
+
     public String home(Model model) {
+
         return "home.html";
+
     }
 }
