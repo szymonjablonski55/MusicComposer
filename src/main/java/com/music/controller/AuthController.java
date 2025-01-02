d05cce8a7b4b6d1a98d4867de5206847382bf471
szymonjablonski55 <jablonskiszymon55@gmail.com>
2025-01-02 18:29:55 +0100

    fix authentication

--- src/main/java/com/music/controller/AuthController.java
+++ src/main/java/com/music/controller/AuthController.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthController {
     @RequestMapping("/login")
-    public String login(Model model) {
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {model.addAttribute("error", "Invalid username or password!");}
         return "login";
     }
     @RequestMapping("/register")
