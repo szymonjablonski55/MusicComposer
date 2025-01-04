53433c0ea08371289f555a2b73c99e40df93f65a
szymonjablonski55 <jablonskiszymon55@gmail.com>
2025-01-04 12:22:43 +0100

    tones

--- src/test/java/com/music/ApplicationTests.java
+++ src/test/java/com/music/ApplicationTests.java
@@ -1,7 +1,28 @@
 package com.music;
 
+import com.music.controller.HomeController;
+import com.music.repository.MusicTrackRepository;
+import com.music.services.MusicService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest
 class ApplicationTests {
